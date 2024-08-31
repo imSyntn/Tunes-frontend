5acafdc9abde5f282678b874df0fdf705a61d5a8
Sayantan Sarkar <imsyntn@gmail.com>
2024-08-31 14:29:11 +0530

    Files Changes

---
+++ src/Components/Header.tsx
@@ -0,0 +1,47 @@
+import { useEffect, useState, useRef, ReactHTMLElement } from 'react'
+import '../Styles/Header.scss'
+import { FaRegUser, FaSearch } from "react-icons/fa";
+
+
+const Header = () => {
+
+    const [searchClicked, setSearchClicked] = useState<Boolean>(false)
+    const inputRef = useRef<HTMLInputElement>(null)
+
+    // useEffect(()=>{
+    //     console.log(searchClicked)
+    // },[searchClicked])
+
+    return (
+        <header>
+            <h1><img src="../../music.png" alt="" />Tunes</h1>
+            <div className={`inputWrapper ${searchClicked ? 'activeInputWrapper' : ''}`} onClick={() => {
+                setSearchClicked(true);
+                inputRef.current?.focus()
+            }}>
+                <div className="top">
+                    <FaSearch />
+                    <p>Search</p>
+                    <input type="text" ref={inputRef} />
+
+                    <svg className='cross' width="20px" height="20px" viewBox="0 0 32 32" version="1.1" xmlns="http://www.w3.org/2000/svg" fill="#000000" onClick={(e) => {
+                        e.stopPropagation()
+                        setSearchClicked(false)
+                    }}><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"><g id="Page-1" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd"> <g id="Icon-Set-Filled" transform="translate(-570.000000, -1089.000000)" fill="#2bc5b4"> <path d="M591.657,1109.24 C592.048,1109.63 592.048,1110.27 591.657,1110.66 C591.267,1111.05 590.633,1111.05 590.242,1110.66 L586.006,1106.42 L581.74,1110.69 C581.346,1111.08 580.708,1111.08 580.314,1110.69 C579.921,1110.29 579.921,1109.65 580.314,1109.26 L584.58,1104.99 L580.344,1100.76 C579.953,1100.37 579.953,1099.73 580.344,1099.34 C580.733,1098.95 581.367,1098.95 581.758,1099.34 L585.994,1103.58 L590.292,1099.28 C590.686,1098.89 591.323,1098.89 591.717,1099.28 C592.11,1099.68 592.11,1100.31 591.717,1100.71 L587.42,1105.01 L591.657,1109.24 L591.657,1109.24 Z M586,1089 C577.163,1089 570,1096.16 570,1105 C570,1113.84 577.163,1121 586,1121 C594.837,1121 602,1113.84 602,1105 C602,1096.16 594.837,1089 586,1089 L586,1089 Z" id="cross-circle"> </path> </g> </g> </g></svg>
+                </div>
+                {
+                    searchClicked && (
+                        <div className='bottom'>
+                            fdjkgdj
+                        </div>
+                    )
+                }
+            </div>
+            <div className="user">
+                <FaRegUser />
+            </div>
+        </header>
+    )
+}
+
+export default Header
